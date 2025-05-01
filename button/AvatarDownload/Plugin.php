6aa6706c55c9f750beee1fd1691fd5001328e3ae
momijiina <93751198+momijiina@users.noreply.github.com>
2025-05-01 11:41:48 +0900

    Create Plugin.php

---
+++ button/AvatarDownload/Plugin.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Plugins\AvatarDownload;
+
+use Exceedone\Exment\Services\Plugin\PluginButtonBase;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+
+class Plugin extends PluginButtonBase
+{
+    /**
+     * Plugin Button
+     */
+    public function execute()
+    {        
+        // 現在ログイン中のユーザーを取得
+        $user = Auth::user();
+
+        // ユーザーのアバター画像のパス
+        $base_path = storage_path('app/admin/' . $user->avatar);
+
+        // ファイルが存在しない場合、デフォルト画像を使用
+        if (!File::exists($base_path)) {
+            $base_path = base_path('public/vendor/exment/images/user.png');
+        }
+
+        // ファイル名を取得
+        $fileName = basename($base_path);
+
+        return [
+            'fileBase64' => base64_encode(File::get($base_path)),
+            'fileContentType' => File::mimeType($base_path),
+            'fileName' => $fileName,
+
+            // 任意：「ダウンロードが完了しました」メッセージを表示する
+            'swaltext' => 'ダウンロードが完了しました',
+        ];
+    }
+}
